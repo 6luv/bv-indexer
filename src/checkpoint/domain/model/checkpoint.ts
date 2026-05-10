98f9aede876fffd5a5cf7b9e91ca52216cac8a02
6luv <6luvev@gmail.com>
2026-05-10 23:59:33 +0900

    feat: add checkpoint

---
+++ src/checkpoint/domain/model/checkpoint.ts
@@ -0,0 +1,48 @@
+import { CheckpointType } from "@/shared/types/checkpoint-type.enum";
+
+export class Checkpoint {
+  constructor(
+    private readonly type: CheckpointType,
+    private readonly lastProcessedBlock: bigint,
+    private readonly updatedAt: number,
+  ) {
+    this.validate();
+  }
+
+  public static create(
+    type: CheckpointType,
+    lastProcessedBlock: bigint,
+    updatedAt: number,
+  ): Checkpoint {
+    return new Checkpoint(type, lastProcessedBlock, updatedAt);
+  }
+
+  private validate(): void {
+    if (
+      this.type !== CheckpointType.BACKFILL &&
+      this.type !== CheckpointType.FORWARDFILL
+    ) {
+      throw new Error("Checkpoint type must be either BACKFILL or FORWARDFILL");
+    }
+
+    if (this.lastProcessedBlock < 0n) {
+      throw new Error("Last processed block must be >= 0");
+    }
+
+    if (this.updatedAt < 0) {
+      throw new Error("Updated at timestamp must be >= 0");
+    }
+  }
+
+  public getType(): CheckpointType {
+    return this.type;
+  }
+
+  public getLastProcessedBlock(): bigint {
+    return this.lastProcessedBlock;
+  }
+
+  public getUpdatedAt(): number {
+    return this.updatedAt;
+  }
+}
